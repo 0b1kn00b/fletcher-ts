f8d8e87a46a50bd759771e955f8e517526a96ec4
0b1kn00b <polysemantic@gmail.com>
2024-05-01 13:00:07 +0200

    fix:module resolution tests#1

--- dist/core/Receiver.js
+++ dist/core/Receiver.js
@@ -1,10 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Receiver = void 0;
 const Cycle_1 = require("./Cycle");
 const Settler_1 = require("./Settler");
 const Apply_1 = require("./Apply");
-const Either = require("fp-ts/Either");
+const Either = __importStar(require("fp-ts/Either"));
 class Receiver extends Settler_1.Settler {
     flat_fold(ok, no) {
         return new Receiver((cont) => {
